f62724ee219b088181030d4a60c19d741744161c
Savannah Urch <Savannah Urch>
2017-03-16 12:12:25 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2urchs77/fileio/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.a2urchs77.fileio;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        String dir_path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        EditText et = (EditText) findViewById(R.id.editText);
+        if (item.getItemId() == R.id.save)
+        {
+
+            try {
+                FileWriter fw = new FileWriter(dir_path + "/notes.txt");
+                PrintWriter pw = new PrintWriter(fw);
+
+
+                pw.println(et.getText());
+                pw.flush();
+                pw.close();
+
+                return true;
+            }
+            catch(IOException e)
+            {
+                System.out.println("ERROR!" + e.getMessage());
+
+            }
+        }
+
+        else if (item.getItemId() == R.id.load)
+        {
+
+            return false;
+
+        }
+
+        return false;
+    }
+
+}
